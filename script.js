f3f9a67676abb426833fb35dd193360a77c05226
oauguste <augusteosnac@gmail.com>
2023-02-26 08:05:30 -0500

    initial project
    inital files

---
+++ script.js
@@ -0,0 +1,23 @@
+const colors = [
+  "green",
+  "red",
+  "rgba(133,122,200)",
+  "#f15025",
+];
+
+const btn = document.getElementById("btn");
+
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  //goal get random number between 0 and 3 colors[2]
+  const randomNumber = getRandomNumber();
+  console.log(randomNumber);
+  document.body.style.backgroundColor =
+    colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
